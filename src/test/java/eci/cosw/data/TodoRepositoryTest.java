fbe565bba5fb923d4cab536e819229b610676b3e
Alejandro Anzola Avila <anzola95avila@gmail.com>
2018-09-23 22:36:25 -0500

    modifica dependencias de pruebas en gradle

--- src/test/java/eci/cosw/data/TodoRepositoryTest.java
+++ src/test/java/eci/cosw/data/TodoRepositoryTest.java
@@ -1,8 +1,8 @@
 package eci.cosw.data;
 
 import eci.cosw.data.model.Todo;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
